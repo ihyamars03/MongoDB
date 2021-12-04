3401ac60505bb43a55c8c544a2bd8d59a7247a53
ihyamars <ihyamars@gmail.com>
2021-12-04 13:05:12 +0700

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require('./utils/db')
 const date = new Date()
 // const tgl = `${date.getDate()}/${date.getUTCMonth() + 1}/${date.getFullYear()}`
 const tgl = date.toLocaleString()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
